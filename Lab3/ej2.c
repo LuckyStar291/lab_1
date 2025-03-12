f3a42a2e48288ad9b5407e95057cb66020198a27
AndresCastellonM <castellon.andresf@gmail.com>
2025-03-11 23:41:47 -0400

    mm

---
+++ Lab3/ej2.c
@@ -0,0 +1,85 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "driverlib/debug.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+
+
+#ifdef DEBUG
+void __error__(char *pcFilename, uint32_t ui32Line)
+{
+    while(1);
+}
+#endif
+
+void state1(void){
+    GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, GPIO_PIN_1); 
+    GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0); 
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_PIN_4); 
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, GPIO_PIN_0);
+
+}
+
+void state2(void){
+    GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, 0); 
+    GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, 0); 
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_4, 0); 
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, 0);
+}
+
+void state3(void){
+    GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, GPIO_PIN_1); 
+    GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, 0); 
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_PIN_4); 
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, 0);
+}
+
+void state4(void){
+    GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, GPIO_PIN_1); 
+    GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0); 
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_4, 0); 
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, 0);
+}
+
+
+int main(void)
+{
+    volatile uint32_t ui32Loop;
+
+    // Habilitar los puertos GPIO para los LEDs y los interruptores
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOJ); // Para los interruptores en PJ0-PJ1
+
+    // Asegurarse de que los puertos GPIO están listos
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION)) {}
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)) {}
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOJ)) {}
+
+    // Configurar los pines de los LEDs como salida
+    GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_4);
+
+    // Configurar los pines de los interruptores como entrada
+    GPIOPinTypeGPIOInput(GPIO_PORTJ_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    // Habilitar las resistencias de Pull-Up para los interruptores
+    GPIOPadConfigSet(GPIO_PORTJ_BASE, GPIO_PIN_0 | GPIO_PIN_1, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+
+    while(1)
+    {
+        state1();
+        SysCtlDelay(120000000/10);
+        state2();
+        SysCtlDelay(120000000/10);
+        state3();
+        SysCtlDelay(120000000/10);
+        state4();
+        SysCtlDelay(120000000/10);
+
+        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
+        {
+        }
+    }
+}
